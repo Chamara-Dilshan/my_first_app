7f02f59aebf9952e951be7a8f5d6750559559e5e
Chamara Dilshan <chamaradilshan0011@gmail.com>
2024-01-26 09:06:44 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
